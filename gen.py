fdb87f62bf44b7b9778a9e78e393dd515f81d23e
Casey MacGibbon <cmacgibbon@smith.edu>
2024-09-15 16:38:31 -0400

    commenting

--- gen.py
+++ gen.py
@@ -22,30 +22,37 @@ class State:
             nexts.append(t.next)
         return nexts
 
-fsa1 = [State([Transition(1, 1), Transition(None, 0), Transition(None, 0)]), 
-        State([Transition(2, .5), Transition(None, 0), Transition(3, .5)]),
-        State([Transition(None, 0), Transition(1, 1), Transition(None, 0)])]
-
-fsa2 = [State([Transition(None, 0), Transition(1, 1), Transition(None, 0)]), 
-        State([Transition(2, .7), Transition(None, 0), Transition(5, .3)]),
-        State([Transition(3, .3), Transition(4, .4), Transition(5, .3)]),
-        State([Transition(3, .5), Transition(None, 0), Transition(5, .5)]),
-        State([Transition(None, 0), Transition(1, 1), Transition(None, 0)])]
-
 def gen(fsa):
+    # initialize word and state index
     word = ""
     pos = 0
     while True:
-       x = random.choices([i for i in range(fsa[pos].length)], weights = fsa[pos].getProbs())[0]
-       if fsa[pos].getNexts()[x] >= len(fsa):
-           break
-       word = word + str(x)
-       pos = fsa[pos].getNexts()[x]
+        # choose randomly from the available transitions based on their probabilities
+        x = random.choices([i for i in range(fsa[pos].length)], weights = fsa[pos].getProbs())[0]
+        # check if in accept state
+        if fsa[pos].getNexts()[x] >= len(fsa):
+            break
+        # append to word
+        word = word + str(x)
+        # update position to next state
+        pos = fsa[pos].getNexts()[x]
     return word
        
 
 def main():
-    for i in range(20):
-        print(gen(fsa2))
+    # 0(01)*
+    fsa1 = [State([Transition(1, 1), Transition(None, 0), Transition(None, 0)]), 
+        State([Transition(2, .5), Transition(None, 0), Transition(3, .5)]),
+        State([Transition(None, 0), Transition(1, 1), Transition(None, 0)])]
+    
+    # 1(011)*0*
+    fsa2 = [State([Transition(None, 0), Transition(1, 1), Transition(None, 0)]), 
+        State([Transition(2, .7), Transition(None, 0), Transition(5, .3)]),
+        State([Transition(3, .3), Transition(4, .4), Transition(5, .3)]),
+        State([Transition(3, .5), Transition(None, 0), Transition(5, .5)]),
+        State([Transition(None, 0), Transition(1, 1), Transition(None, 0)])]
+    
+    print(gen(fsa1))
+    print(gen(fsa2))
 
 main()
