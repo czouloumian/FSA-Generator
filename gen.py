ed49dbd0dd9210b7c6840df146a58f089aa44bad
Karenna Kung <95237168+kkung9@users.noreply.github.com>
2024-10-03 19:10:25 -0400

    make english sounding words

--- gen.py
+++ gen.py
@@ -23,9 +23,17 @@ class State:
         return nexts
 
 def gen(fsa):
+    letters = [["a", "e", "i", "o", "u"], # vowels
+               ["ai", "au", "ea", "ee", "ei", "oa", "oi", "ou", "oo"], # double vowels
+               ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "r", "s", "t", "x", "z", "th", "ch", "sh", "st", "ll", "ss"], # anywhere
+               ["j", "v", "w", "y"], # anywhere but end
+               ["sl", "tr", "cr", "br", "sm", "dr", "qu"]] # start only
+    
+
     # initialize word and state index
     word = ""
     pos = 0
+    # count = 0
     while True:
         # choose randomly from the available transitions based on their probabilities
         x = random.choices([i for i in range(fsa[pos].length)], weights = fsa[pos].getProbs())[0]
@@ -33,26 +41,28 @@ def gen(fsa):
         if fsa[pos].getNexts()[x] >= len(fsa):
             break
         # append to word
-        word = word + str(x)
+        word = word + str(letters[x][random.choices([i for i in range(len(letters[x]))])[0]])
         # update position to next state
         pos = fsa[pos].getNexts()[x]
+        # count += 1
     return word
        
 
 def main():
-    # 0(01)*
-    fsa1 = [State([Transition(1, 1), Transition(None, 0), Transition(None, 0)]), 
-        State([Transition(2, .5), Transition(None, 0), Transition(3, .5)]),
-        State([Transition(None, 0), Transition(1, 1), Transition(None, 0)])]
+
+    fsa3 = [State([Transition(3, .25), Transition(None, 0), Transition(4, .4), Transition(4, .1), Transition(4, .25), Transition(None, 0)]),
+            State([Transition(2, .4), Transition(2, .2), Transition(None, 0), Transition(None, 0), Transition(None, 0), Transition(6, .4)]),
+            State([Transition(None, 0), Transition(None, 0), Transition(1, 0.6), Transition(1, 0.2), Transition(None, 0), Transition(6, .2)]),
+            State([Transition(None, 0), Transition(None, 0), Transition(4, 0.8), Transition(4, 0.2), Transition(None, 0), Transition(None, 0)]),
+            State([Transition(5, .7), Transition(5, .3), Transition(None, 0), Transition(None, 0), Transition(None, 0), Transition(None, 0)]),
+            State([Transition(None, 0), Transition(None, 0), Transition(1, .7), Transition(1, .3), Transition(None, 0), Transition(None, 0)]),]
     
-    # 1(011)*0*
-    fsa2 = [State([Transition(None, 0), Transition(1, 1), Transition(None, 0)]), 
-        State([Transition(2, .7), Transition(None, 0), Transition(5, .3)]),
-        State([Transition(3, .3), Transition(4, .4), Transition(5, .3)]),
-        State([Transition(3, .5), Transition(None, 0), Transition(5, .5)]),
-        State([Transition(None, 0), Transition(1, 1), Transition(None, 0)])]
-    
-    print(gen(fsa1))
-    print(gen(fsa2))
+    for i in range(30):
+        print(gen(fsa3))
 
 main()
+
+# cv(cvc)*
+
+# not ending with certain letters
+# general complexity in a syllable
