1da28d2ea35b8d9a69b0a744a983092a68d1781e
Karenna Kung <95237168+kkung9@users.noreply.github.com>
2024-10-08 13:09:49 -0400

    minor clean up

--- gen.py
+++ gen.py
@@ -33,7 +33,6 @@ def gen(fsa):
     # initialize word and state index
     word = ""
     pos = 0
-    # count = 0
     while True:
         # choose randomly from the available transitions based on their probabilities
         x = random.choices([i for i in range(fsa[pos].length)], weights = fsa[pos].getProbs())[0]
@@ -44,25 +43,19 @@ def gen(fsa):
         word = word + str(letters[x][random.choices([i for i in range(len(letters[x]))])[0]])
         # update position to next state
         pos = fsa[pos].getNexts()[x]
-        # count += 1
     return word
        
 
 def main():
 
-    fsa3 = [State([Transition(3, .25), Transition(None, 0), Transition(4, .4), Transition(4, .1), Transition(4, .25), Transition(None, 0)]),
+    fsa = [State([Transition(3, .25), Transition(None, 0), Transition(4, .4), Transition(4, .1), Transition(4, .25), Transition(None, 0)]),
             State([Transition(2, .4), Transition(2, .2), Transition(None, 0), Transition(None, 0), Transition(None, 0), Transition(6, .4)]),
             State([Transition(None, 0), Transition(None, 0), Transition(1, 0.6), Transition(1, 0.2), Transition(None, 0), Transition(6, .2)]),
             State([Transition(None, 0), Transition(None, 0), Transition(4, 0.8), Transition(4, 0.2), Transition(None, 0), Transition(None, 0)]),
             State([Transition(5, .7), Transition(5, .3), Transition(None, 0), Transition(None, 0), Transition(None, 0), Transition(None, 0)]),
             State([Transition(None, 0), Transition(None, 0), Transition(1, .7), Transition(1, .3), Transition(None, 0), Transition(None, 0)]),]
     
-    for i in range(30):
-        print(gen(fsa3))
+    for i in range(10):
+        print(gen(fsa))
 
 main()
-
-# cv(cvc)*
-
-# not ending with certain letters
-# general complexity in a syllable
